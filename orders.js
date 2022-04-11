cdbbba962f76769cb83bc0ec25f1e794654555f1
Tyler Hankerson <94148274+TsunamiTy@users.noreply.github.com>
2022-04-11 16:23:23 -0400

    profile photo update

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'eCommerce Store',
+        productNumber: '85631',
+        payment: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Mobile App',
+        productNumber: '36378',
+        payment: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Personal Website',
+        productNumber: '49347',
+        payment: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'eCommerce Store',
+        productNumber: '27132',
+        payment: 'Paid',
+        status: 'Complete'
+    },
+    {
+        productName: 'Business Website',
+        productNumber: '39116',
+        payment: 'Paid',
+        status: 'Complete'
+    },
+    {
+        productName: 'Landing Page',
+        productNumber: '22821',
+        payment: 'Paid',
+        status: 'Complete'
+    },
+    {
+        productName: 'Blog Site',
+        productNumber: '44829',
+        payment: 'Due',
+        status: 'Pending'
+    }
+]
